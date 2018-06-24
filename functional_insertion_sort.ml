4319f2288d1912659c1e65f3c20af786cadc1fed
Martina Lui <martina.lui@gmail.com>
2018-06-24 16:51:40 -0700

    Finish functional style insertion sort algorithm.

--- functional_insertion_sort.ml
+++ functional_insertion_sort.ml
@@ -1,11 +1,15 @@
-(* insert takes a sorted list s and an element h and returns a sorted list that orders elements from the smallest to the biggest. *)
-let rec insert s h =
+(* sort l takes a list and returns a sorted list that orders elements from the smallest to the biggest. *)
+let rec sort l = 
+  (* insert takes a sorted list s and an element h and returns a sorted list that orders elements from the smallest to the biggest. *)
+  let rec insert s h =
   match s with
-    shd :: stl -> if shd > h then
-                    h :: shd :: stl
-                  else
-                    shd :: insert stl h
+    shd :: stl -> 
+      if shd > h then
+       h :: shd :: stl
+      else
+       shd :: insert stl h
     |_ -> [h]
-;;
-
-
+  in
+  match l with 
+    [] -> []
+    | hd :: tl -> insert (sort tl ) hd
