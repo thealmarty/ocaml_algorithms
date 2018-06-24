79e0555fed69dc11866f941ba2fd4708995d2c32
Martina Lui <martina.lui@gmail.com>
2018-06-24 15:07:57 -0700

    Add the case of empty list in function insert.

--- functional_insertion_sort.ml
+++ functional_insertion_sort.ml
@@ -1,11 +1,11 @@
-(* insert takes a list s and an element h and returns a sorted list that orders elements from the smallest to the biggest. *)
+(* insert takes a sorted list s and an element h and returns a sorted list that orders elements from the smallest to the biggest. *)
 let rec insert s h =
   match s with
     shd :: stl -> if shd > h then
                     h :: shd :: stl
                   else
                     shd :: insert stl h
+    |_ -> [h]
 ;;
 
 
-    
