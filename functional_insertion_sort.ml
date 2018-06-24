6e45cf8fcf7d3be9b69da68892a8f9f39b8b4a9a
Martina Lui <martina.lui@gmail.com>
2018-06-24 14:22:30 -0700

    Add comments.

--- functional_insertion_sort.ml
+++ functional_insertion_sort.ml
@@ -1,3 +1,4 @@
+(* insert takes a list s and an element h and returns a sorted list that orders elements from the smallest to the biggest. *)
 let rec insert s h =
   match s with
     shd :: stl -> if shd > h then
--- imperative_insertion_sort.ml
+++ imperative_insertion_sort.ml
@@ -1,3 +1,4 @@
+(* Insertion sort algorithm written in imperative style, strictly following the psuedocode on p.18 of the book "Introduction to Algorithms". Run in toplevel.*)
 let insertion_sort a =
   for j = 1 to Array.length a - 1 do
     let key = a.(j) and i = ref (j - 1) in
