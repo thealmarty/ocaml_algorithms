84215603b9038cb006653f6687efebac828f9b02
Martina Lui <martina.lui@gmail.com>
2018-06-26 14:41:27 -0700

    selection_sort works.

--- imperative_selection_sort.ml
+++ imperative_selection_sort.ml
@@ -2,10 +2,11 @@
 let selection_sort a =
     for j = 0 to Array.length a -1 do 
     let tmp = ref 0 in
-      let themin = ref j and i = ref (Array.length a - 1) in
-        while !i >= j && a.(!i) < a.(!themin) do
-          themin := !i ;
-          i := !i - 1
+      let themin = ref j in
+        for i = j to Array.length a - 1 do
+          while a.(i) < a.(!themin) do
+            themin := i ;
+          done;
         done;
       tmp := a.(!themin) ;
       a.(!themin) <- a.(j) ;
