8fe404b9793c0fd97e875882cae407225ad6e52d
Martina Lui <martina.lui@gmail.com>
2018-06-26 14:33:36 -0700

    Still not sorting properly.

--- imperative_selection_sort.ml
+++ imperative_selection_sort.ml
@@ -1,15 +1,14 @@
 (* Selection sort algorithm written in imperative style, solving exercise 2.2-2 on p.29 of the book "Introduction to Algorithms". Run in toplevel.*)
 let selection_sort a =
-  for j = 0 to Array.length a - 1 do
-    let min = ref j and i = ref (Array.length a - 1 - j) in
-      while !i > 0 && a.(!i) < a.(!min) do
-        a.(!i) <- a.(!min) ;
-        min := !i ;
-        i := !i - 1
-      done;
-    a.(j) <- a.(!min)
-  done
-;;
-
-
-    
+    for j = 0 to Array.length a -1 do 
+    let tmp = ref 0 in
+      let themin = ref j and i = ref (Array.length a - 1) in
+        while !i >= j && a.(!i) < a.(!themin) do
+          themin := !i ;
+          i := !i - 1
+        done;
+      tmp := a.(!themin) ;
+      a.(!themin) <- a.(j) ;
+      a.(j) <- !tmp ;
+    done
+;; 
