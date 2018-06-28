456144dac6f292e8083162e20dd801e685844c47
Martina Lui <martina.lui@gmail.com>
2018-06-28 12:27:16 -0700

    Move select out of sort.

--- sort/functional_selection_sort.ml
+++ sort/functional_selection_sort.ml
@@ -1,16 +1,38 @@
+(* select takes a list s and an element h and returns a list that is either
+ * unchanged, if h is smaller than or equal to the smallest element of h, or
+ * with the smallest element of s replaced by h.  
+ * Either h or the smallest element of s is then the input to the function sort.*)
 let rec select s h =
   match s with
     shd :: stl ->
       (match stl with 
         stlhd :: stltl ->
           if shd < h then
-            shd :: h :: select (List.tl stl) (List.hd stl)
+            h :: select stl shd
           else
-            h :: shd :: select (List.tl stl) (List.hd stl)
+            shd :: select stl h
         |_ ->
             if shd < h then
-              shd :: [h]
+              [h] 
             else 
-              h :: s )
+              [shd]
+      )
     |_ -> []
+;; 
 
+let rec sort l =
+   match l with
+      [] -> []
+      | hd :: tl -> 
+        match tl with
+          [] -> []
+      |_ ->
+        let rec findmin n =
+            if List.nth tl n = List.nth (select tl hd) n then
+              (if n < List.length tl - 1 then findmin (n + 1)
+              else sort tl)
+            else 
+              List.nth tl :: sort tl
+          in
+          findmin (0)
+;;
