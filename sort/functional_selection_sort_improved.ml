649761103f6c4ab8239afa8dfc4d7a83cd21fdd7
Martina Lui <martina.lui@gmail.com>
2018-07-02 11:39:50 -0700

    Write improved version of selection sort which more closely follow the algorithm than makesortedlist.

---
+++ sort/functional_selection_sort_improved.ml
@@ -0,0 +1,30 @@
+(* findmin takes a list and returns the smallest element of the list.*)
+let rec findmin (l : 'a list) : 'a =
+  match l with
+    hd :: h2 :: tl ->
+      if hd < h2 then
+        findmin (hd :: tl)
+      else
+        findmin (h2 :: tl)
+    |hd :: tl ->
+      hd 
+;; 
+
+(* remaining_list takes a list and returns a list with the smallest element of the origin list taken out.*)
+let rec remaining_list (l : 'a list) : 'a list =
+  match l with
+    hd :: h2 :: tl ->
+      if hd < h2 then
+        h2 :: remaining_list (hd :: tl)
+      else
+        hd :: remaining_list (h2 :: tl)
+    |_ -> []
+;; 
+
+(* selection_sort takes a list and sort the list using the selection sort algorithm.*)
+let selection_sort (l : 'a list) : 'a list =
+  match l with
+    hd :: tl ->
+      findmin l :: selection_sort (remaining_list l)
+    |_ -> l
+;;
