dd2251da9e38e9a9d587cd91293424d5d40bf650
Martina Lui <martina.lui@gmail.com>
2018-07-02 10:45:33 -0700

    Clean up select to only taking a list as argument.

--- sort/functional_selection_sort.ml
+++ sort/functional_selection_sort.ml
@@ -1,40 +1,13 @@
-(* select takes a list s and an element h and returns a list that is either
- * unchanged, if h is smaller than or equal to the smallest element of h, or
- * with the smallest element of s replaced by h.  
- * Either h or the smallest element of s is then the input to the function sort.*)
-let rec select s h =
+(* select takes a list s and returns a list with all the same elements of s but the smallest element of s is the last element of the list.*)
+let rec select s =
   match s with
-    shd :: stl ->
-      (match stl with 
-        stlhd :: stltl ->
-          if shd < h then
-            h :: select stl shd
-          else
-            shd :: select stl h
-        |_ ->
-            if shd < h then
-              [h] 
-            else 
-              [shd]
-      )
-    |_ -> []
-;; 
-
-let rec findmin l = 
-  match l with
-    shd :: stl ->
-      (match stl with 
-        stlhd :: stltl ->
-          if shd < stlhd then
-            select stl shd
-          else
-            select stl stlhd
-        |_ ->
-            if shd < List.hd stl then
-              shd :: findmin (select stl shd)
-            else 
-              List.hd stl :: findmin (select stl shd)
-      )
+    shd :: sh2 :: stl ->
+      if shd < sh2 then
+        sh2 :: select (shd :: stl)
+      else
+        shd :: select (sh2 :: stl)
+    |shd :: stl ->
+      [shd] 
     |_ -> []
 ;; 
 
