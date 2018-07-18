aa9e2c061fe588010edd76af1aeedfdacf145198
Martina Lui <martina.lui@gmail.com>
2018-07-18 14:09:19 -0700

    First try of imperative style merge sort algorithm.

--- sort/imperative_merge_sort.ml
+++ sort/imperative_merge_sort.ml
@@ -1,3 +1,4 @@
+(* Merge is written according to the pseudocode in "Introduction to Algorithms" 3rd edition p.31. *)
 let merge a p q r =
   let n1 = ref (q - p + 1) and n2 = ref (r - q) in
     let la = Array.make (!n1 + 1) max_int and ra = Array.make (!n2 + 1) max_int in
@@ -18,3 +19,13 @@ let merge a p q r =
           done
 ;;
 
+(* merge_sort is written following the pseudocode on p.34 of the same book, using merge as a subroutine. *)
+let rec merge_sort a p r =
+  let q = ref 0 in
+    while p < r do
+      q := (p + r)/2;
+      merge_sort a p q;
+      merge_sort a (q + 1) r;
+      merge a p q r
+    done
+;;
