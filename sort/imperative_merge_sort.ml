3b4978d19bb2c5fbf174a7fe3b3e9f77bab39e56
Martina Lui <martina.lui@gmail.com>
2018-07-18 13:52:08 -0700

    Imperative merge seems to work. Next step is get it working recursively and fully implement the merge sort algorithm.

--- sort/imperative_merge_sort.ml
+++ sort/imperative_merge_sort.ml
@@ -1,11 +1,11 @@
 let merge a p q r =
   let n1 = ref (q - p + 1) and n2 = ref (r - q) in
-    let la = Array.make !n1 max_int and ra = Array.make !n2 max_int in
+    let la = Array.make (!n1 + 1) max_int and ra = Array.make (!n2 + 1) max_int in
       for i = 0 to !n1 - 1 do
-        la.(i) <- a.(p + i - 2)
+        la.(i) <- a.(p + i - 1)
       done;
       for j = 0 to !n2 - 1 do
-        ra.(j) <- a.(q + j - 1)
+        ra.(j) <- a.(q + j)
       done;
         let i = ref 0 and j = ref 0 in
           for k = p - 1 to r - 1 do
